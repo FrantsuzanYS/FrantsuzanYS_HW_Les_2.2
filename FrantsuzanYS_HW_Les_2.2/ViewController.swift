8d1304c355710d8cc494998febae550497146c88
Frantsuzan_YS <niajulya@icloud.com>
2021-07-10 11:10:08 +0300

    Initial Commit

---
+++ FrantsuzanYS_HW_Les_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrantsuzanYS_HW_Les_2.2
+//
+//  Created by emmisar on 10.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
