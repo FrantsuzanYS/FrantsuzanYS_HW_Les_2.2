b48bdbb97aaf544435c9cd3767f6727fe710c6f1
Frantsuzan_YS <niajulya@icloud.com>
2021-07-10 18:33:36 +0300

    add actions for elements

--- FrantsuzanYS_HW_Les_2.2/ViewController.swift
+++ FrantsuzanYS_HW_Les_2.2/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        paintBackground()
     }
 
+    @IBAction func colorSliderAction(_ sender: UISlider) {
+        
+        let sliderValue = round(sender.value * 100) / 100
+        
+        switch sender {
+        case redSlider:
+            redValueLabel.text = String(sliderValue)
+        case greenSlider:
+            greenValueLabel.text = String(sliderValue)
+        default:
+            blueValueLabel.text = String(sliderValue)
+        }
 
+        paintBackground()
+    }
+    
+    private func paintBackground(){
+       
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),                                              green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+        
+    }
 }
 
